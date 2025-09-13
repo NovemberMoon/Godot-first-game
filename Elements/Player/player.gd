dfd499139c19221b29d5eaccb05e7141430b9a38
NovemberMoon <novsunv@gmail.com>
2025-09-13 19:41:51 +0300

    feat: add AnimationPlayer Node for Player

--- Elements/Player/player.gd
+++ Elements/Player/player.gd
@@ -5,6 +5,7 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 @onready var anim = $AnimatedSprite2D
+@onready var animPlayer = $AnimationPlayer
 
 var health = 100
 var gold = 0
@@ -18,18 +19,18 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("attack") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		anim.play('Jump')
+		animPlayer.play('Jump')
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 		if velocity.y == 0:
-			anim.play("Run")
+			animPlayer.play("Run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		if velocity.y == 0:
-			anim.play("Idle")
+			animPlayer.play("Idle")
 		
 	if direction == -1:
 		anim.flip_h = true
@@ -37,7 +38,7 @@ func _physics_process(delta: float) -> void:
 		anim.flip_h = false
 	
 	if velocity.y > 0:
-		anim.play("Fall")
+		animPlayer.play("Fall")
 		
 	if health <= 0:
 		queue_free()
